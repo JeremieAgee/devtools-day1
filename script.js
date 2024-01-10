f0849bcb76d79ecd7686e402ffe1bf97497820fa
JeremieAgee <you@example.com>
2024-01-09 20:13:09 -0600

    finished debugging

--- script.js
+++ script.js
@@ -21,9 +21,10 @@ function displayOutput() {
 // Error 6: Misusing variable scope
 function updateMessage() {
   let localMessage = 'Updated message';
+  console.log(localMessage); 
 }
 updateMessage();
-console.log(localMessage); 
+
 
 // Error 7:
 document.getElementById('testButton').addEventListener('click', testFunction());
@@ -39,11 +40,11 @@ console.log('Sum is: ' + (number1 - number2));
 
 // Error 9: 
 let nullVar = null;
-console.log(nullVar.length);
+console.log(nullVar);
 
 // Error 10:
 let caseSensitiveVar = 'Case matters';
-console.log(CaseSensitiveVar);
+console.log(caseSensitiveVar);
 
 // Error 11:
 let importantData =
@@ -51,7 +52,7 @@ let importantData =
 console.log('importantData');
 
 // Error 12: 
-tryToCallFunction();
+// tryToCallFunction();
 
 // Error 13:
 let user = { namee: 'Alice' };
@@ -59,13 +60,13 @@ console.log(user.name)
 
 // Error 14: 
 let someNumber = 123;
-console.log(someNumber.toUpperCase()); 
+console.log(someNumber); 
 
 // Error 15: 
 let greeting = "Hello, world";
 
 // Error 16: 
-for (let i = 0; i < 5; i++); {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
@@ -77,16 +78,18 @@ if (five === 5) {
     console.log('Five is not equal to 5'); 
 }
 // Error 18:
-Array.prototype.push = function() {
-    console.log('Array push method is overwritten');
-};
 let numbers = [];
 numbers.push(1);
 console.log(numbers);
+Array.prototype.push = function() {
+    console.log('Array push method is overwritten');
+};
+
 
 // Error 19:
-console.log(x);
 x = 5;
+console.log(x);
+
 
 // Error 20:
 let myString = {};
